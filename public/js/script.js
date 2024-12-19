b29f55f7a77825c1ead6ba78756f2965905c34ce
Aurélien St <aurelien.strimelle@bstorm.be>
2024-12-19 15:47:37 +0100

    Demo 02 - Encapsulation 🐚

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,33 @@
 "use strict";
 console.log("Demo Orienté objet en TS");
 class House {
-    desc;
-    nbRoom;
-    hasGarden;
+    _desc;
+    _nbRoom;
+    _hasGarden;
     constructor(nbRoom, desc, hasGarden) {
-        this.nbRoom = nbRoom;
-        this.desc = desc ?? 'Aucune information';
-        this.hasGarden = hasGarden ?? false;
+        this._nbRoom = nbRoom;
+        this._desc = desc ?? 'Aucune information';
+        this._hasGarden = hasGarden ?? false;
+    }
+    get nbRoom() {
+        return this._nbRoom;
+    }
+    get desc() {
+        return this._desc;
+    }
+    get hasGarden() {
+        return this._hasGarden;
+    }
+    set nbRoom(value) {
+        if (value < 1) {
+            throw new Error('Il doit avoir une piece mini !');
+        }
+        this._nbRoom = value;
+    }
+    set desc(value) {
+        this._desc = value;
     }
 }
+const h1 = new House(5);
+console.log(h1.desc);
+h1.desc = "test";
--- src/script.ts
+++ src/script.ts
@@ -5,12 +5,17 @@ console.log("Demo Orienté objet en TS");
 //  Pour cela, on va créer des "classes" (Blue print) avec des props et méthodes
 //  Pour les manipulers, il sera necessaire d'instancier les classes -> Objet
 
+//! Encapsulation
+//  Protéger les données dans nos objets
+//  - Limiter l'acces via des méthodes (Getter / Setter)
+//  - Changer la visibilité des propriétés
+
 class House {
 
     // Déclaration des propriétés de la classe "Maison"
-    desc: string;
-    nbRoom: number;
-    hasGarden: boolean;
+    private _desc: string;
+    private _nbRoom: number;
+    private _hasGarden: boolean;
 
     // Méthode « Constructeur »
     //? ↓ Les signatures possibles de constructeur (Polymorphisme / Just for dev)
@@ -20,8 +25,42 @@ class House {
     //? ↓ L'implémentation réel du constructeur
     constructor(nbRoom: number, desc?: string, hasGarden?: boolean) {
         // La logique pour initialiser les propriétés
-        this.nbRoom = nbRoom;
-        this.desc = desc ?? 'Aucune information';
-        this.hasGarden = hasGarden ?? false;
+        this._nbRoom = nbRoom;
+        this._desc = desc ?? 'Aucune information';
+        this._hasGarden = hasGarden ?? false;
     }
+
+    // Méthode « Getter / Setter »
+    //? Getter
+    get nbRoom() : number {
+        return this._nbRoom;
+    }
+    
+    get desc() : string {
+        return this._desc;
+    }
+    
+    get hasGarden() : boolean {
+        return this._hasGarden;
+    }
+
+    //? Setter
+    set nbRoom(value: number) {
+        if(value < 1) {
+            // Erreur si la valeur est invalide
+            throw new Error('Il doit avoir une piece mini !');
+        }
+        this._nbRoom = value;
+    }
+    set desc(value: string) {
+        this._desc = value;
+    }
+
+
 }
+
+
+const h1 = new House(5);
+console.log(h1.desc) 
+h1.desc = "test";
+// h1.#hasGarden = false;
