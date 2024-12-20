1c81d9a3273214c85a3f9f23988670f2f7370e75
Aurélien St <aurelien.strimelle@bstorm.be>
2024-12-20 14:48:42 +0100

    Demo 11 - Le générique

--- public/js/script3.js
+++ public/js/script3.js
@@ -1,2 +1,33 @@
 "use strict";
 console.warn("Le générique en TS");
+function tabFilterNumber(tab, callbackFilter) {
+    const result = [];
+    for (const val of tab) {
+        if (callbackFilter(val)) {
+            result.push(val);
+        }
+    }
+    return result;
+}
+function tabFilterString(tab, callbackFilter) {
+    const result = [];
+    for (const val of tab) {
+        if (callbackFilter(val)) {
+            result.push(val);
+        }
+    }
+    return result;
+}
+function tabFilter(tab, callbackFilter) {
+    const result = [];
+    for (const val of tab) {
+        if (callbackFilter(val)) {
+            result.push(val);
+        }
+    }
+    return result;
+}
+const t1 = ['Riri', 'Fifi', 'Loulou'];
+const r1 = tabFilter(t1, (name) => name.includes('i'));
+const t2 = [52, 10, 9, 16, 11, 42];
+const r2 = tabFilter(t2, (nb) => nb % 2 === 0);
--- src/script3.ts
+++ src/script3.ts
@@ -1,1 +1,58 @@
 console.warn("Le générique en TS");
+
+
+function tabFilterNumber(tab: number[], callbackFilter : (nb: number) => boolean) : number[] {
+
+    const result : number[] = [];
+
+    for(const val of tab) {
+
+        if(callbackFilter(val)) {
+            result.push(val);
+        }
+    }
+
+    return result;
+}
+
+function tabFilterString(tab: string[], callbackFilter : (nb: string) => boolean) : string[] {
+
+    const result : string[] = [];
+
+    for(const val of tab) {
+
+        if(callbackFilter(val)) {
+            result.push(val);
+        }
+    }
+
+    return result;
+}
+
+
+//! Generique 
+//  Permet de créer un "parametre" pour définir un type de donnée à utiliser
+//  Ce type doit être passer entre crochet oblique <...>
+//  Il est possible de rendre des fonctions, des classes et des interfaces génériques
+
+function tabFilter<TElement>(tab: TElement[], callbackFilter : (nb: TElement) => boolean) : TElement[] {
+
+    const result : TElement[] = [];
+
+    for(const val of tab) {
+
+        if(callbackFilter(val)) {
+            result.push(val);
+        }
+    }
+
+    return result;
+}
+
+
+const t1 = ['Riri', 'Fifi', 'Loulou']
+const r1 = tabFilter<string>(t1, (name) => name.includes('i'));
+
+const t2 = [52, 10, 9, 16, 11, 42];
+const r2 = tabFilter(t2, (nb) => nb % 2 === 0);
+
