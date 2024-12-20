16f83a03b8f3307be1178372607d69b36b243930
Aurélien St <aurelien.strimelle@bstorm.be>
2024-12-20 13:37:46 +0100

    Demo 07 - Interfaces 🪟

--- public/js/script2.js
+++ public/js/script2.js
@@ -1,2 +1,34 @@
 "use strict";
 console.warn("Les interfaces en TS");
+class Person {
+    _lastname;
+    _firstname;
+    constructor(firstname, lastname) {
+        this._firstname = firstname;
+        this._lastname = lastname;
+    }
+    get firstname() {
+        return this._firstname;
+    }
+    set firstname(value) {
+        this._firstname = value;
+    }
+    get lastname() {
+        return this._lastname;
+    }
+    set lastname(value) {
+        this._lastname = value;
+    }
+    get fullname() {
+        return `${this.firstname} ${this.lastname}`;
+    }
+    sleep(nbHour) {
+        return `${this.fullname} dort ${nbHour} heures`;
+    }
+    eat(meal) {
+        if (Math.random() > 0.9) {
+            return `${this.fullname} refuse de manger !`;
+        }
+        return `${this.fullname} mange ${meal}`;
+    }
+}
--- src/script2.ts
+++ src/script2.ts
@@ -1,1 +1,63 @@
 console.warn("Les interfaces en TS");
+
+//! Les interfaces
+//  L'ensembles des contraintes (Getters, Setters, Méthodes)
+//  qu'une classe va devoir respecté.
+//  On peut voir l'interface comme un "contrat" que la classe passe.
+//  Celle-ci contient les signatures des élément sans l'implémenté.
+
+interface IPerson {
+    // Propriétés
+    firstname: string;
+    lastname: string;
+    fullname: string;
+    // Méthodes
+    sleep(nbHour: number) : string;
+    eat(meal: string) : string;
+}
+
+class Person implements IPerson {
+
+    // Props
+    private _lastname: string;
+    private _firstname: string;
+
+    // Ctor
+    constructor(firstname: string, lastname: string) {
+        this._firstname = firstname;
+        this._lastname = lastname;
+    }
+
+    // Encapsulation    
+    public get firstname(): string {
+        return this._firstname;
+    }
+    public set firstname(value: string) {
+        this._firstname = value;
+    }
+
+    public get lastname(): string {
+        return this._lastname;
+    }
+    public set lastname(value: string) {
+        this._lastname = value;
+    }
+
+    public get fullname() : string {
+        return `${this.firstname} ${this.lastname}`;
+    }
+
+    // Méthodes
+    sleep(nbHour: number): string {
+        return `${this.fullname} dort ${nbHour} heures`;
+    }
+
+    eat(meal: string): string {
+        if(Math.random() > 0.9) {
+            return `${this.fullname} refuse de manger !`;
+        }
+
+        return `${this.fullname} mange ${meal}`;
+    }
+
+}
